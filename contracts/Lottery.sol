9def60523ccffb62c60b487fce5bd829b5427dbd
Feed-dev <federico.lubbert@gmail.com>
2023-05-05 16:10:16 +0200

    more on 'Lottery' contract

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -8,15 +8,29 @@
 
 pragma solidity ^0.8.7;
 
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+
 error Raffle__NotEnoughEthEntered();
 
-contract Raffle {
+contract Raffle is VRFConsumerBaseV2 {
     /* State Variables */
     uint256 private immutable i_entranceFee;
     address payable[] private s_players;
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    bytes32 private immutable i_gasLane;
 
-    constructor(uint256 entranceFee) {
+    /* Events */
+    event RaffleEnter(address indexed player);
+
+    constructor(
+        address vrfCoordinatorV2,
+        uint256 entranceFee,
+        bytes32 gasLane
+    ) VRFConsumerBaseV2(vrfCoordinatorV2) {
         i_entranceFee = entranceFee;
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
+        i_gasLane = gasLane;
     }
 
     function enterRaffle() public payable {
@@ -25,10 +39,30 @@ contract Raffle {
             revert Raffle__NotEnoughEthEntered();
         }
         s_players.push(payable(msg.sender));
+        //Emit an event when we update a dynamic array or mapping
+        //Named events with the function name reversed
+        emit RaffleEnter(msg.sender);
     }
 
-    //function pickRandomWinner() {}
+    function requestRandomWinner() external {
+        //request a random number
+        //use the random number
+        //2 transaction process
+        i_vrfCoordinator.requestRandomWords(
+            i_gasLane,
+            s_subscriptionId,
+            requestConfirmations,
+            callbackGasLimit,
+            numWords
+        );
+    }
 
+    function fulfillRandomWords(
+        uint256 requestId,
+        uint256[] memory randomWords
+    ) internal override {}
+
+    /* View / Pure functions */
     function getEntranceFee() public view returns (uint256) {
         return i_entranceFee;
     }
